cabea646eefb3f115548d247ee1f8561d76762c3
Damian Kisielewski <damian@gravitichain.com>
2019-01-16 14:16:02 +0000

    Basic menu structure

---
+++ src/Components/Menu/Menu.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './Menu.scss';
+
+const Menu = () => {
+  return (
+    <ul className="Menu">
+      <li className="Menu__item">Login</li>
+      <li className="Menu__item">Download IOS App</li>
+      <li className="Menu__item">Download Android App</li>
+    </ul>
+  );
+}
+
+export default Menu;
