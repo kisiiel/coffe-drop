bbecdc56f85e39863c68aa686e09744118173817
Damian Kisielewski <damian@gravitichain.com>
2019-01-16 11:32:30 +0000

    Initial project setup

---
+++ src/CoffeDrop.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './CoffeDrop.scss';
+
+class CoffeDrop extends Component {
+  render() {
+    return (
+      <div className="CoffeDrop">
+
+      </div>
+    );
+  }
+}
+
+export default CoffeDrop;
