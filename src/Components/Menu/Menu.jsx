d85dcc0beb258c8061d0bb923a570034c9ad068f
Damian Kisielewski <damian@gravitichain.com>
2019-01-17 08:33:54 +0000

    responsive menu

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
-
+import MenuButton from '../../assets/hamburgermenu.svg';
 import './Menu.scss';
 
 const Menu = () => {
   return (
-    <ul className="Menu">
-      <li className="Menu__item">Login</li>
-      <li className="Menu__item">Download IOS App</li>
-      <li className="Menu__item">Download Android App</li>
-    </ul>
+    <>
+      <button className="Menu__button"><img className="Menu__buttonIcon" src={MenuButton} alt="Show menu"/></button>
+      <ul className="Menu">
+        <li className="Menu__item">Login</li>
+        <li className="Menu__item">Download IOS App</li>
+        <li className="Menu__item">Download Android App</li>
+      </ul>
+    </>
+
   );
 }
 
